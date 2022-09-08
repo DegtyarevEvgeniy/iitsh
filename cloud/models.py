f35e2c1f9d6abf9a267fb4ad93be38fe561698b5
evgin <evgin@pop-os.localdomain>
2022-09-08 19:24:23 +0300

    add some changes

--- cloud/models.py
+++ cloud/models.py
@@ -1,3 +1,11 @@
+import uuid
 from django.db import models
+from django.forms import ModelForm
+from taggit.managers import TaggableManager
+from datetime import datetime
 
-# Create your models here.
+class Cabinet(models.Model):
+    number = models.IntegerField()
+    subject = models.CharField(max_length=100)
+    sensors = models.CharField(max_length=100)
+
