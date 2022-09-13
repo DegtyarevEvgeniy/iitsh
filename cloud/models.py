aa3fbc0365dde965ef39e21eed5e98a4c3ce103f
emiliamam <emilka20052015@icloud.com>
2022-09-13 23:17:08 +0300

    models

--- cloud/models.py
+++ cloud/models.py
@@ -5,7 +5,20 @@ from taggit.managers import TaggableManager
 from datetime import datetime
 
 class Cabinet(models.Model):
-    number = models.IntegerField()
-    subject = models.CharField(max_length=100)
-    sensors = models.CharField(max_length=100)
+    number = models.IntegerField(max_length=100)
+    time = models.DateTimeField()
+    state = models.IntegerField()
+    alarm = models.IntegerField(max_length=1)
+    temperature = models.IntegerField(max_length=100)
+    humidity = models.IntegerField(max_length=100)
+    pressure = models.IntegerField(max_length=1000)
+    lux = models.IntegerField(max_length=5000)
+    co2 = models.IntegerField(max_length=5000)
+    co = models.IntegerField(max_length=5000)
+    gas = models.IntegerField(max_length=5000)
+    movement = models.IntegerField(max_length=1)
+    door = models.IntegerField(max_length=1)
+
+
+
 
